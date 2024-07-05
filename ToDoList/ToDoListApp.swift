5cbc2040e4007936f1f64f187fe341c6402b1dc2
graceco22 <graceco921@gmail.com>
2024-07-04 22:12:28 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Grace Co on 2024-07-04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
